af9bcaa9a2901c46d51ec21ac918fa2cc13d54ba
Tomiprase3270 <tomi.prase3270@gmail.com>
2022-09-27 23:28:33 +0800

    change authentication using session and cookies with passport.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 //jshint esversion:6
-require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-// const md5 = require("md5");
-// const encrypt = require('mongoose-encryption');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 
 const app = express();
 
@@ -15,24 +14,36 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// use express session 
+app.use(session({
+    secret: "Our Little Secret",
+    resave: false,
+    saveUninitialized: false
+}));
 
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // build mongodb database with mongoose
 mongoose.connect("mongodb://localhost:27017/user", { useNewUrlParser: true });
+// mongoose.set("useCreateIndex", true);
 
 const newUserSchema = new mongoose.Schema({
     username: String,
     password: String
 });
 
-
-// // encrypt paswword using mongoose-encryption
-// newUserSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] })
+newUserSchema.plugin(passportLocalMongoose);
 
 
 // mongoose model to save user after encrypted
 const User = mongoose.model("User", newUserSchema);
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // render home page
 app.get("/", (req, res) => {
@@ -48,53 +59,54 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function (req, res, next) {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect("/")
+    });
+});
 
 // POST for register
 app.post("/register", (req, res) => {
-
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-        username = req.body.username;
-        password = hash;
-
-        const newUser = new User({
-            username: username,
-            password: password
-        });
-
-        newUser.save(function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-        });
+    User.register({ username: req.body.username }, req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
     });
 });
 
 
-
-// POST for login
 app.post("/login", (req, res) => {
-    username = req.body.username;
-    password = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({ username: username }, (err, foundUser) => {
-
+    req.login(user, function (err) {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password, function (err, result) {
-                    if (result === true) {
-                        res.render("secrets")
-                    } else {
-                        console.log(err);
-                    }
-                });
-            }
-        };
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
+        }
     });
 });
+
+
 
 app.listen(3000, (err) => {
     if (!err) {
